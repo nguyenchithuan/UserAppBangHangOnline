80e72b854e6c08457a86d6dc53378f36ff404c02
haule0110 <hauvanle2003@gmail.com>
2023-10-02 02:34:43 +0700

    Hau 2/ DS SP

---
+++ app/src/main/java/edu/wkd/userappbanghangonline/ultil/CheckConection.java
@@ -0,0 +1,29 @@
+package edu.wkd.userappbanghangonline.ultil;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+public class CheckConection {
+    public static boolean HaveConnection(Context context){
+        boolean wifi = false, moblie = false;
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] infos = cm.getAllNetworkInfo();
+        for (NetworkInfo networkInfo : infos){
+            if (networkInfo.getTypeName().equals("WIFI"))
+                if (networkInfo.isConnected())
+                    wifi = true;
+
+            if (networkInfo.getTypeName().equals("MOBILE"))
+                if (networkInfo.isConnected())
+                    moblie = true;
+        }
+        return wifi || moblie;
+    }
+
+    public static void ShowToast(Context context, String tb){
+        Toast.makeText(context, tb, Toast.LENGTH_SHORT).show();
+    }
+
+}
