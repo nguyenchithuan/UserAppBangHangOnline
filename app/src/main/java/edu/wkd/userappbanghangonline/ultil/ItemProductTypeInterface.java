af1827c4e66cb35ad483f10dea7f390e79491dc5
haule0110 <hauvanle2003@gmail.com>
2023-10-11 00:52:16 +0700

    Hau 10/10 Gettypelist

---
+++ app/src/main/java/edu/wkd/userappbanghangonline/ultil/ItemProductTypeInterface.java
@@ -0,0 +1,7 @@
+package edu.wkd.userappbanghangonline.ultil;
+
+import edu.wkd.userappbanghangonline.model.obj.ProductType;
+
+public interface ItemProductTypeInterface {
+    void onClickItemProduct(ProductType producttype);
+}
