0c467200bb47352daf270c459b4ad8eb645c5f1b
linhdvph <dovanlinh20102003@gmail.com>
2023-10-24 12:16:01 +0700

    linh 24/10 update product reviews activity

--- app/src/main/java/edu/wkd/userappbanghangonline/view/activity/ProductReviewsActivity.java
+++ app/src/main/java/edu/wkd/userappbanghangonline/view/activity/ProductReviewsActivity.java
@@ -3,6 +3,7 @@ package edu.wkd.userappbanghangonline.view.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RatingBar;
 
 import edu.wkd.userappbanghangonline.R;
 import edu.wkd.userappbanghangonline.databinding.ActivityProductReviewsActivityBinding;
@@ -14,5 +15,33 @@ public class ProductReviewsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityProductReviewsActivityBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        setTitleWhenChooseStar();//Hiển thị tiêu đề phù hợp với từng sao được chọn
+    }
+
+    private void setTitleWhenChooseStar() {
+        binding.rattingBarProductReviews.setRating(5);
+        binding.rattingBarProductReviews.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                switch ((int) rating){
+                    case 1:
+                        binding.tvTitleByRating.setText("Tệ");
+                        break;
+                    case 2:
+                        binding.tvTitleByRating.setText("Không hài lòng");
+                        break;
+                    case 3:
+                        binding.tvTitleByRating.setText("Bình thường");
+                        break;
+                    case 4:
+                        binding.tvTitleByRating.setText("Hài lòng");
+                        break;
+                    case 5:
+                        binding.tvTitleByRating.setText("Tuyệt vời");
+                        break;
+                }
+            }
+        });
     }
 }
